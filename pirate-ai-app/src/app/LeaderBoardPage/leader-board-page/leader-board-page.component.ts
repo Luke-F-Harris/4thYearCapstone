8985664573b2d84a93383c65b9e14b92e462d4e6
abdin20 <naymul@hotmail.ca>
2021-11-23 17:23:47 -0500

    tried adding mat table

--- pirate-ai-app/src/app/LeaderBoardPage/leader-board-page/leader-board-page.component.ts
+++ pirate-ai-app/src/app/LeaderBoardPage/leader-board-page/leader-board-page.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
-import { DataSource } from '@angular/cdk/table';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 
+export interface playerInfo {
+  name: string;
+  rank: number;
+  picLink: string;
+  lastSubmission: string;
+}
 
 
 @Component({
@@ -14,18 +19,23 @@ export class LeaderBoardPageComponent implements OnInit {
   
 
   //array holding top players
-  public topPlayers:Array<any>;
+  public topPlayers:playerInfo[]=[];
+  displayedColumns: string[] = ['name', 'rank', 'picLink', 'lastSubmission'];
+  public dataSource: MatTableDataSource<playerInfo>;
   constructor() { 
-    this.topPlayers=[];
+    
+    this.topPlayers.push({name:"bob",rank:1,picLink:"https://i.stack.imgur.com/l60Hf.png",lastSubmission:"1 day ago"});
+    this.topPlayers.push({name:"alice",rank:2,picLink:"https://i.stack.imgur.com/l60Hf.png",lastSubmission:"1 day ago"});
+    this.dataSource=new MatTableDataSource(this.topPlayers);
   }
 
   ngOnInit(): void {
 
+    
+
     //replace code here with db fetching top players
 
-    this.topPlayers.push({name:"bob",rank:1,picLink:"https://i.stack.imgur.com/l60Hf.png",lastSubmission:"1 day ago"});
-    this.topPlayers.push({name:"alice",rank:2,picLink:"https://i.stack.imgur.com/l60Hf.png",lastSubmission:"1 day ago"});
-
+   
   }
 
 }
