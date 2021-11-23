9601df108cc13467ed57ffec025fd06b6d13b033
andrewharrop <andrewhar05@gmail.com>
2021-11-23 18:03:06 -0500

    db pop

--- api/database/config/db_populate.js
+++ api/database/config/db_populate.js
@@ -1,1 +1,17 @@
-// Populate the database with samples. This is for later
+// Populate the database with samples.
+const fs = require('fs');
+const db = require('../connect');
+
+['users.sql', 'codes.sql', 'games.sql'].forEach(file => {
+    fs.readFile(`sql/${file}`, 'utf8', (err, data) => {
+        if (err) throw err;
+        data = data.split('\n');
+        data.forEach(line => {
+            if (line.length > 0) {
+                db.query(line);
+            }
+        });
+    });
+})
+
+// db.end();
