f7136cb928fec042633774350ea850984ced30f8
Luke Harris <ildosh123@hotmail.com>
2021-11-25 19:00:35 -0500

    mad changes

--- pirate-ai-app/src/app/HomePage/home-page/home-page.component.ts
+++ pirate-ai-app/src/app/HomePage/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -10,6 +10,16 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll() {
+  //In chrome and some browser scroll is given to body tag
+  let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+  let max = document.documentElement.scrollHeight;
+  // pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
+   if(pos == max )   {
+   //Do your action here
+   }
   }
 
 }
