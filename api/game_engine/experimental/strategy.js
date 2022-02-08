64c97e6ea364c19babdfb517ee07fc93464a4b8f
andrewharrop <andrewhar05@gmail.com>
2022-02-08 15:19:14 -0500

    Added to the game map

---
+++ api/game_engine/experimental/strategy.js
@@ -0,0 +1,20 @@
+// A well defined class-object structure can be used define strategy objects and functions.
+
+// Define the constants
+const StaticConstants = require('../pirate_game/StaticConstants');
+
+// Define the geometry items
+const Geometry = require('../pirate_game/Geometry');
+
+
+// We need a game map parameter to host the interaction space.
+
+/*
+    General strategy structure:
+    {gameMap}
+
+*/
+
+function sampleStrategy(gameMap) {
+    // If there are ships that are undocked, we should dock them to the nearest island.
+}
