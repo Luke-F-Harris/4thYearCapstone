ada8367cc696eea1f17032fb315548e4fad2982d
abdin20 <naymul@hotmail.ca>
2022-03-04 18:38:38 -0500

    added navigation

--- Capstone/Unity-Capstone/Assets/ShipDocking.cs
+++ Capstone/Unity-Capstone/Assets/ShipDocking.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class ShipDocking : MonoBehaviour
 {
     public bool isDocked = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        //simulated movement until docked
-        if(!isDocked){  
-        this.transform.position = new Vector3(this.transform.position.x+0.001f, this.transform.position.y, this.transform.position.z);
-        }
+
+     
+   
+      
+      
+
+      
     }
 }
