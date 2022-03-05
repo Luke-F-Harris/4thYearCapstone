a7b62809b19af8ab50717f0d9a2d584211e01a30
andrewharrop <andrewhar05@gmail.com>
2022-03-05 15:19:44 -0500

    tt

--- api/routing/game.js
+++ api/routing/game.js
@@ -22,7 +22,7 @@ let game_map = {
 
 module.exports = function (app) {
     app.get("/api/games/all", (req, res, next) => {
-        games.query(games.games(), (err, res) => {
+        games.query(games.games(), (err, result) => {
             if (err) {
                 logger.error(err);
                 res.status(500);
@@ -31,7 +31,7 @@ module.exports = function (app) {
                 });
             } else {
                 res.status(200);
-                res.json(res);
+                res.json(result);
             }
         });
     });
