aca43aeebbaa9cae242753909682f41799a2a738
abdin20 <naymul@hotmail.ca>
2022-03-05 16:18:09 -0500

    fixed post request on game build

--- Capstone/Unity-Capstone/runWebGL.bat
+++ Capstone/Unity-Capstone/runWebGL.bat
@@ -1,3 +1,4 @@
 "C:\Program Files\Unity\Hub\Editor\2020.3.28f1\Editor\Unity.exe" -quit -batchmode  -projectPath %cd% -executeMethod GameBuilder.WebGLProductionBuild -logFile "./log.txt"
-curl -X POST http://localhost:3000/api/games/end -H 'Content-Type: application/json' -d '{"index_file_path":"C:/Capstone/Builds"}'
+curl --header "Content-Type: application/json" --request POST --data "{\"index_file_path\":\"C:/Capstone/Builds\"}" http://localhost:3000/api/games/end
 pause
+
