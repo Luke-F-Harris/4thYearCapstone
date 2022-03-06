a1e923730435399020e55547d43c65f6f502dccd
Riley <roverlan@uwo.ca>
2022-03-06 16:37:40 -0500

    fixing unity stuff

--- Capstone/Unity-Capstone/Assets/Uploads/code_6.cs
+++ Capstone/Unity-Capstone/Assets/Uploads/code_6.cs
@@ -1,1 +1,45 @@
-qwsdqwed
+// // const express = require('express');
+// // require('dotenv').config();
+// // const bcrypt = require('bcrypt');
+// // const jwt = require('jsonwebtoken');
+
+// // require('./config/database').connect();
+// // const user = require('./model/user');
+
+// // const app = express();
+
+// // app.use(express.json());
+
+// // app.post("/register", (req, res) => {
+    // // const { first_name, last_name, email, password } = req.body;
+
+    // // if (!(email && password && first_name && last_name)) {
+        // // res.status(400).send("all input is required");
+    // // }
+    // // const olduser = user.findone({ email });
+    // // if (olduser) {
+        // // res.status(409).send("user already exists");
+    // // }
+    // // const salt = bcrypt.gensaltsync(10);
+    // // const hash = bcrypt.hashsync(password, salt);
+    // // const user = new user({
+        // // first_name,
+        // // last_name,
+        // // email,
+        // // password: hash
+    // // });
+    // // const token = jwt.sign(
+        // // { id: user._id, email: email },
+        // // process.env.token,
+        // // { expiresin: '1h' }
+    // // );
+    // // user.token = token;
+
+// // });
+
+// // app.post("/login", (req, res) => {
+    // // // our login logic goes here
+// // });
+
+// // module.exports = app;
+
