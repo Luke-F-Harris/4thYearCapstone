61496172a14e2b7b3af1267ed56f8db564b1e317
abdin20 <naymul@hotmail.ca>
2022-03-05 20:16:30 -0500

    fixed pathing

--- Capstone/Unity-Capstone/runWebGL.bat
+++ Capstone/Unity-Capstone/runWebGL.bat
@@ -1,5 +1,5 @@
-"C:\Program Files\Unity\Hub\Editor\2020.3.28f1\Editor\Unity.exe" -quit -batchmode  -projectPath %cd% -executeMethod GameBuilder.WebGLProductionBuild -logFile "./log.txt"
-curl --header "Content-Type: application/json" --request POST --data "{\"index_file_path\":\"C:/Capstone/Builds\"}" http://localhost:3000/api/games/end
+"C:\Program Files\Unity\Hub\Editor\2020.3.28f1\Editor\Unity.exe" -quit -batchmode  -projectPath "../Capstone/Unity-Capstone" -executeMethod GameBuilder.WebGLProductionBuild -logFile "./log.txt"
+curl --header "Content-Type: application/json" --request POST --data "{\"index_file_path\":\"C:/Capstone/Builds/4\"}" http://localhost:3000/api/games/end
 pause
 
 
